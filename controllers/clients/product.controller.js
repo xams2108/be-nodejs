eb1c0107177dc633e8f722e0c3becb667e0f7df4
Trần Đỗ Tiến Đạt <xams2108@gmail.com>
2025-02-26 19:25:14 +0700

    sửa layout default

--- controllers/clients/product.controller.js
+++ controllers/clients/product.controller.js
@@ -1,6 +1,6 @@
 module.exports.index = (req,res)=>{
     console.log("kết nối với /products/")
     res.render('client/pages/products/index.pug',{
-        title: "Sản phẩm"
+        title: "Danh Mục Sản phẩm"
     })
 }
