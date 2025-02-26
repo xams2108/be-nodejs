04a8664c46e10a740f1628cce08b38bcf70ba80f
Trần Đỗ Tiến Đạt <xams2108@gmail.com>
2025-02-26 17:42:55 +0700

    cấu hình file .env

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
+require('dotenv').config();
 const route = require("./routes/client/index.route") // import router để sài
 
 const app = express();
-const port = 300;
+const port = process.env.PORT;
 app.set('views','views')//set views ở file view
 app.set('view engine', "pug")//set engine 
 
