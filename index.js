a2b3988de5be5d2de6f8ec40d1cd314cd779d2bd
Trần Đỗ Tiến Đạt <xams2108@gmail.com>
2025-02-26 18:10:04 +0700

    add static file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 require('dotenv').config();
-const route = require("./routes/client/index.route") // import router để sài
-
+const route = require("./routes/client/index.route"); // import router để sài
 const app = express();
+app.use(express.static("public"));
 const port = process.env.PORT;
-app.set('views','views')//set views ở file view
-app.set('view engine', "pug")//set engine 
+app.set('views','views');//set views ở file view
+app.set('view engine', "pug");//set engine 
 
 route(app)
 
