d3e93c07a1543165aff0486244005b7d9dd29e24
Trần Đỗ Tiến Đạt <xams2108@gmail.com>
2025-03-01 21:20:58 +0700

    Tối ưu bộ lộc code gọn hơn

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,43 +1,16 @@
 //Get /admin/products
 const Products = require("../../model/products.model")
+const FilterStatus =  require("../../helper/filterStatus")
 module.exports.index = async (req,res)=>{
-
-    //filter button start
-    let filterStatus =[
-        {
-            name:"Tất cả",
-            status: "",
-            class: ""
-        },
-        {
-            name:"Hoạt động",
-            status: "active",
-            class:""
-        },
-        {
-            name:"không hoạt động",
-            status: "inactive",
-            class:""
-        }
-    
-    ]
-    if(req.query.status){
-        const index = filterStatus.findIndex(item => item.status == req.query.status)
-        filterStatus[index].class = "active"
-    }else{
-        const index = filterStatus.findIndex(item => item.status == "")
-        filterStatus[index].class = "active"
-    }
-
-    console.log(req.query.status)
     const find = {
         deleted: false
     }
-
+    filterStatus = FilterStatus(req.query)
+    //filter status start
     if(req.query.status){ //request status có giá trị không nếu có thì trả về giá trị status để lọc sản pẩm
         find.status = req.query.status
     }
-    //filter button start
+    //filter status end
 
 
     //tìm kiếm bằng keyword start
@@ -46,13 +19,14 @@ module.exports.index = async (req,res)=>{
     }
     //tìm kiếm bằng keyword end
 
-
-
+    const keyword = req.query.keyword
     const products = await Products.find(find);
+
     res.render("admin/pages/products/index.pug",{
         title: "products",
         text: "Danh sach sản phẩm",
         products: products,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        keyword: keyword
     });
 }
--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const systemdata = require("./config/system")
 require('dotenv').config();
 
 database.connect();
+const app = express();
 const routeclient = require("./routes/client/index.route"); // import router client để sài
 const routeadmin = require("./routes/admin/index.route");
-const app = express();
+routeclient(app)
+routeadmin(app)
+
 app.use(express.static("public"));
 const port = process.env.PORT;
 app.set('views','views');//set views ở file view
 app.set('view engine', "pug");//set engine 
+
+
 app.locals.admin = systemdata.PathAdmin
-routeclient(app)
-routeadmin(app)
+
 
 app.listen(port,() =>{
     console.log(`Project opening on port:${port}`)
